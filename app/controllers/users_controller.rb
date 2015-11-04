38bb327900f5654813f8450f8139b75ab000386a
Ted Shumway <shumway.ted@gmail.com>
2015-11-03 17:15:25 -0800

    user update profile, added flash notice, and you can only update your own profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,9 +10,29 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user == current_user
+      if @user.update(user_params)
+        flash[:notice] = "You have successfully updated profile."
+        redirect_to root_path
+      else
+        render :update
+      end
+    else
+      flash[:notice] = "unauthorized user"
+      redirect_to root_path
+    end
+  end
+
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       redirect_to root_path
     else
       render :new
